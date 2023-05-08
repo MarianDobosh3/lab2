09143d840af0d4fda10223040d5ca6b9898a7dde
MarianDobosh3 <127721946+MarianDobosh3@users.noreply.github.com>
2023-05-08 09:51:02 +0300

    Update pairs.py

--- lab_3/pairs.py
+++ lab_3/pairs.py
@@ -2,5 +2,5 @@ numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] # створюємо масив
 
 for i in range(len(numbers)): # робимо крок з довжиною в numbers, тобто 10
     for j in range(i+1, len(numbers)): # додаємо +1 допоки довжина numbers не закінчиться
-        if numbers[i] + numbers[j] == 10: # умова, що змінні i і j дорівнює 10, то виводиться наступні дії
-            print(numbers[i], "+", numbers[j])
+        if numbers[i] + numbers[j] == 10: # умова, що змінні i і j дорівнює 10, то виконуються наступні дії >
+            print(numbers[i], "+", numbers[j]) # виводимо змінні i і j з знаком "+" між ними
